5e3d6808d2089b3ca827ac310d371c4a792ddc63
qalqa <andreybas04@gmail.com>
2025-03-20 17:13:40 +0300

    fix: fxied id from string to uuid

--- webcoursesite/app/models.py
+++ webcoursesite/app/models.py
@@ -1,14 +1,15 @@
+import uuid
 from django.db import models
 
 
 class Person(models.Model):
-    id = models.CharField(max_length=50, primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=20)
     middle_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     contact = models.CharField(max_length=50)
     role = models.CharField(max_length=50)
-    img = models.ImageField(upload_to="")
+    img = models.ImageField()
 
     class Meta:
         app_label = "app"
