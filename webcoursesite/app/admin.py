746b6fcac603d09f8d21d683ff23b9d416103163
qalqa <andreybas04@gmail.com>
2025-03-20 19:50:46 +0300

    feat: added all urls to admin

--- webcoursesite/app/admin.py
+++ webcoursesite/app/admin.py
@@ -1,9 +1,49 @@
 from django.contrib import admin
-from .models import Person
+from .models import *
 
 
 class PersonAdmin(admin.ModelAdmin):
     exclude = ("img",)
 
 
+class TeacherAdmin(PersonAdmin):
+    pass
+
+
+class MentorAdmin(PersonAdmin):
+    pass
+
+
+class UsefulPostAdmin(admin.ModelAdmin):
+    pass
+
+
+class LabAdmin(admin.ModelAdmin):
+    pass
+
+
+class TestAdmin(admin.ModelAdmin):
+    pass
+
+
+class ScheduleAdmin(admin.ModelAdmin):
+    pass
+
+
+class LectureAdmin(admin.ModelAdmin):
+    pass
+
+
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Person, PersonAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Mentor, MentorAdmin)
+admin.site.register(UsefulPost, UsefulPostAdmin)
+admin.site.register(Lab, LabAdmin)
+admin.site.register(Test, TestAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
+admin.site.register(Lecture, LectureAdmin)
+admin.site.register(Course, CourseAdmin)
